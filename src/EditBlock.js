6a003fc7463e3ed298cbf5cd63eb6e60bb1aa46e
Leo <losoviz@gmail.com>
2020-03-07 12:46:29 +0800

    Import graphiql.css

--- src/EditBlock.js
+++ src/EditBlock.js
@@ -1,24 +1,27 @@
 import React, { Component } from "react";
 import GraphiQL from 'graphiql';
 import fetch from 'isomorphic-fetch';
+import 'graphiql/graphiql.css';
 import { __ } from '@wordpress/i18n';
 
 class EditBlock extends Component {
 
     graphQLFetcher(graphQLParams) {
-        return fetch(window.location.origin + '/graphql', {
+        return fetch(window.location.origin + '/api/graphql', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(graphQLParams),
         }).then(response => response.json());
     }
 
-
-  render() {
-	return (
-		<GraphiQL fetcher={ this.graphQLFetcher } />
-	);
-  }
+    render() {
+        return (
+            <GraphiQL
+                style={{ height: '80vh' }}
+                fetcher={ this.graphQLFetcher }
+            />
+        );
+    }
 }
 
 export default EditBlock;
