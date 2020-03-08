2c9e1265955a304d7ca3ddcedc8e897627a42721
Leo <losoviz@gmail.com>
2020-03-08 15:41:13 +0800

    Allow to modify as HTML

--- src/index.js
+++ src/index.js
@@ -83,8 +83,8 @@ registerBlockType( 'leoloso/graphiql', {
 		align: ['center', 'wide', 'full'],
 		// Remove the support for the custom className.
 		customClassName: false,
-		// Remove support for an HTML mode.
-		html: false,
+		// // Remove support for an HTML mode.
+		// html: false,
 		// // Only insert block through a template
 		// inserter: false,
 		// // Only one per CPT is needed
